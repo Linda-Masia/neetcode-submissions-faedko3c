7ebc431ee707ebee6954ce03970a805e06169568
Linda Masia <133004091+Linda-Masia@users.noreply.github.com>
2026-06-23 14:46:16 +0200

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        // 1. Clean the string using StringBuilder
+        StringBuilder cleaned = new StringBuilder();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char curr = s.charAt(i);
+            // Check if it's an alphanumeric character
+            if (Character.isLetterOrDigit(curr)) {
+                cleaned.append(Character.toLowerCase(curr));
+            }
+        }
+        
+        // 2. Convert back to string for your palindrome check
+        String filteredStr = cleaned.toString();
+        int sLen = filteredStr.length();
+
+        for (int i = 0; i < sLen / 2; i++) {
+            if (filteredStr.charAt(i) != filteredStr.charAt(sLen - i - 1)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
